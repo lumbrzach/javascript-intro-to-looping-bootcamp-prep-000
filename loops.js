ac440c3294dd66194c9e6a07ae303cc34af0f4de
Zach Parker <parkerz410@gmail.com>
2019-05-03 18:47:43 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -15,3 +15,15 @@ function whileLoop(n) {
     }
     return 'done'
 }
+
+var i = 0;
+ 
+function incrementVariable() {
+  i = i + 1;
+  return i;
+}
+
+do {
+  console.log("I run once regardless.")
+} while (incrementVariable()< num) {
+}
