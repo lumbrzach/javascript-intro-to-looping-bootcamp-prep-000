66c802fff257a6631386cae23a31533a420892ea
Zach Parker <parkerz410@gmail.com>
2019-05-03 19:10:27 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -16,16 +16,17 @@ function whileLoop(n) {
     return 'done'
 }
 
+
+
+function doWhileLoop(num) {
 var i = 0;
 
 function incrementVariable() {
   i = i + 1;
   return i;
 }
-
-function doWhileLoop(num) {
   do {
-    console.log("I run once regardless.")
+    console.log("I run once regardless.");
   } while (incrementVariable()< num);
   return num
 }
